cda2c51f14eefe4af26e5f5cb6546418a6888493
kobbybawuah <Bawuah10@hotmail.com>
2023-09-10 03:47:14 -0400

    reversing fix. causing unexplainable behaviour

--- app/page.jsx
+++ app/page.jsx
@@ -6,16 +6,16 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   //short cut around webgl issue of not being able to render more than 4 canvas in chrome
-  const [isChromeOrSafari, setIsChromeOrSafari] = useState(false);
+  // const [isChromeOrSafari, setIsChromeOrSafari] = useState(false);
 
-  useEffect(() => {
-    const userAgent = window.navigator.userAgent;
-    const isChrome = /Chrome/.test(userAgent);
-    const isSafari = /Safari/.test(userAgent) && !/Chrome/.test(userAgent);
-    if (isChrome || isSafari) {
-      setIsChromeOrSafari(true);
-    }
-  }, []);
+  // useEffect(() => {
+  //   const userAgent = window.navigator.userAgent;
+  //   const isChrome = /Chrome/.test(userAgent);
+  //   const isSafari = /Safari/.test(userAgent) && !/Chrome/.test(userAgent);
+  //   if (isChrome || isSafari) {
+  //     setIsChromeOrSafari(true);
+  //   }
+  // }, []);
 
   return (
     <div>
@@ -32,7 +32,8 @@ export default function Home() {
         {/* <Feedbacks /> */}
         <div className='relative z-0'>
           <Contact />
-          {!isChromeOrSafari && <StarsCanvas />}
+          {/* {!isChromeOrSafari && <StarsCanvas />} */}
+          <StarsCanvas />
         </div>
         <div>game</div>
       </div>
