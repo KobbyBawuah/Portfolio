f2eee6f9c6bf00597d34e59a6a2f3ec6b2445d67
kobbybawuah <Bawuah10@hotmail.com>
2023-09-10 01:20:25 -0400

    done state before game implementation

--- components/canvas/Earth.jsx
+++ components/canvas/Earth.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useLoader } from "@react-three/fiber";
+
+import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  return (
-    <div>Earth</div>
-  )
-}
+  const earth = useGLTF("./planet/scene.gltf");
+  const gltf = useLoader(GLTFLoader, "/earth2/scene.gltf");
 
-export default Earth
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={10}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={50} />
+      <primitive object={gltf.scene} scale={2.5} position-y={0} rotation-y={0} />
+    </mesh>
+  );
+};
+
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      // shadows
+      frameloop='demand'
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 10,
+        near: 0.01,
+        far: 10,
+        position: [-4, 3, 6],
+      }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+        // maxPolarAngle={Math.PI / 2}
+        // minPolarAngle={Math.PI / 2}
+        />
+        <Earth />
+
+        <Preload all />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default EarthCanvas;
