669b9dee97d680e2c515363552269c07f9531b01
kobbybawuah <Bawuah10@hotmail.com>
2023-09-11 23:08:15 -0400

    editing main text for mobiles to close space

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -18,10 +18,10 @@ const Hero = ({ isMobile }) => {
         </div>
 
         <div>
-          <h1 className={`font-black text-white lg:text-[60px] sm:text-[40px] xs:text-[35px] text-[40px] lg:leading-[98px] mt-2 text-white`}>
+          <h1 className={`font-black text-white lg:text-[60px] sm:text-[40px] xs:text-[60px] text-[40px] lg:leading-[98px] mt-2 text-white`}>
             Hello, I'm <span className='text-[#915EFF]'>Kobby</span>
           </h1>
-          <p className={`text-[#dfd9ff] font-medium lg:text-[25px] sm:text-[21px] xs:text-[18px] text-[16px] lg:leading-[40px] mt-2 text-white-100`}>
+          <p className={`text-[#dfd9ff] font-medium lg:text-[25px] sm:text-[21px] xs:text-[35px] text-[16px] lg:leading-[40px] mt-2 text-white-100`}>
             I develop applications to
             address everyday challenges whilst
             integrating cutting-edge technologies
@@ -32,23 +32,6 @@ const Hero = ({ isMobile }) => {
 
       {!isMobile && <ComputersCanvas />}
 
-      {/* <a href='#about'>
-        <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
-        </div>
-      </a> */}
     </section>
 
   )
